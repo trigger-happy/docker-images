3a9bf13b86fae560971f5f611c9e10bb61c1f900
James Choa <triggerhappyelite@gmail.com>
2022-11-24 20:39:17 +0800

    fix: mlflow image not building and update package

--- mlflow-server/docker-entrypoint.sh
+++ mlflow-server/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 exec mlflow server \
   --host $HOST \
   --port $PORT \
-  --backend-store-uri $BACKEND_STORE_URI \
+  --backend-store-uri file://$BACKEND_STORE_URI \
   --default-artifact-root $DEFAULT_ARTIFACT_ROOT \
   --workers $WORKERS \
   --expose-prometheus $METRICS_DIR
