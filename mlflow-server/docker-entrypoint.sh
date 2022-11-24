12c8642decdd92ea17eacdee7a149126a7bc7ddc
James Choa <triggerhappyelite@gmail.com>
2022-11-25 00:42:40 +0800

    feat: switch to using env vars instead

--- mlflow-server/docker-entrypoint.sh
+++ mlflow-server/docker-entrypoint.sh
@@ -2,10 +2,4 @@
 
 mkdir -p /data/mlruns /data/metrics /data/artifacts
 
-exec mlflow server \
-  --host $HOST \
-  --port $PORT \
-  --backend-store-uri $BACKEND_STORE_URI \
-  --default-artifact-root $DEFAULT_ARTIFACT_ROOT \
-  --workers $WORKERS \
-  --expose-prometheus $METRICS_DIR
+exec mlflow server
