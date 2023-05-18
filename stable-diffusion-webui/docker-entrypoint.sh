98fbfe2b9181b7efc47e6030369ccecb12e22e81
James Choa <triggerhappyelite@gmail.com>
2023-05-19 00:33:36 +0800

    Add no-half-vae arg

--- stable-diffusion-webui/docker-entrypoint.sh
+++ stable-diffusion-webui/docker-entrypoint.sh
@@ -4,7 +4,7 @@ cd stable-diffusion-webui
 cp -rf packaged_models/* models/
 
 if [[ -n "${ENABLE_INSECURE_ACCESS}" && "${ENABLE_INSECURE_ACCESS}" == "true" ]]; then
-  python launch.py --listen --port $PORT --enable-insecure-extension-access
+  python launch.py --listen --port $PORT --no-half-vae --enable-insecure-extension-access
 else
-  python launch.py --listen --port $PORT
+  python launch.py --listen --port $PORT --no-half-vae
 fi
