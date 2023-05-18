b243b33d47d2e04658f7c8f8891dbfdc379193e8
James Choa <triggerhappyelite@gmail.com>
2023-05-18 17:09:41 +0800

    Add flag for extension installation

--- stable-diffusion-webui/docker-entrypoint.sh
+++ stable-diffusion-webui/docker-entrypoint.sh
@@ -2,4 +2,9 @@
 
 cd stable-diffusion-webui
 cp -rf packaged_models/* models/
-python launch.py --listen --port $PORT
+
+if [[ -n "${ENABLE_INSECURE_ACCESS}" && "${ENABLE_INSECURE_ACCESS}" == "true" ]]; then
+  python launch.py --listen --port $PORT --enable-insecure-extension-access
+else
+  python launch.py --listen --port $PORT
+fi
