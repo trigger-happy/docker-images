9c1c7abf34ae56df6a4e665693f23b941862521f
James Choa <triggerhappyelite@gmail.com>
2023-06-28 01:53:59 +0800

    Add jupyterlab and jupyter_ai

--- text-generation-webui/docker-entrypoint.sh
+++ text-generation-webui/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd text-generation-webui
+#python jupyter lab --ip='*' --NotebookApp.token='' --NotebookApp.password='' &
 python server.py --listen --listen-port $PORT --auto-devices --gpu-memory $GPUMEMORY --no-cache --cpu-memory $CPUMEMORY --threads $THREADS --rwkv-cuda-on --monkey-patch --extensions openai
