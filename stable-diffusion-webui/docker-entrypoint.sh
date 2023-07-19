96db2e9f9cf11d77712b0f6072c9084ed776db1f
James Choa <triggerhappyelite@gmail.com>
2023-07-19 21:55:05 +0800

    Enable the api

--- stable-diffusion-webui/docker-entrypoint.sh
+++ stable-diffusion-webui/docker-entrypoint.sh
@@ -4,7 +4,7 @@ cd stable-diffusion-webui
 cp -rf packaged_models/* models/
 
 if [[ -n "${ENABLE_INSECURE_ACCESS}" && "${ENABLE_INSECURE_ACCESS}" == "true" ]]; then
-  python launch.py --listen --port $PORT --no-half-vae --enable-insecure-extension-access --xformers
+  python launch.py --listen --port $PORT --no-half-vae --enable-insecure-extension-access --xformers --api
 else
-  python launch.py --listen --port $PORT --no-half-vae --xformers
+  python launch.py --listen --port $PORT --no-half-vae --xformers --api
 fi
