bab927b2f0c55ec294275dd9ea222cc996f96d3b
James Choa <triggerhappyelite@gmail.com>
2023-07-19 12:08:57 +0800

    Add xformers flag

--- stable-diffusion-webui/docker-entrypoint.sh
+++ stable-diffusion-webui/docker-entrypoint.sh
@@ -4,7 +4,7 @@ cd stable-diffusion-webui
 cp -rf packaged_models/* models/
 
 if [[ -n "${ENABLE_INSECURE_ACCESS}" && "${ENABLE_INSECURE_ACCESS}" == "true" ]]; then
-  python launch.py --listen --port $PORT --no-half-vae --enable-insecure-extension-access
+  python launch.py --listen --port $PORT --no-half-vae --enable-insecure-extension-access --xformers
 else
-  python launch.py --listen --port $PORT --no-half-vae
+  python launch.py --listen --port $PORT --no-half-vae --xformers
 fi
