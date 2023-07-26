52c89696d1efe978290d9c12aac9abf0c3800a96
James Choa <triggerhappyelite@gmail.com>
2023-07-26 10:13:28 +0800

    Load a model on startup through the env var
    Needed to actually load the config files properly for
    instruct models.

--- text-generation-webui/docker-entrypoint.sh
+++ text-generation-webui/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd text-generation-webui
 #python jupyter lab --ip='*' --NotebookApp.token='' --NotebookApp.password='' &
-python server.py --listen --listen-port $PORT --auto-devices --gpu-memory $GPUMEMORY --no-cache --cpu-memory $CPUMEMORY --threads $THREADS --rwkv-cuda-on --monkey-patch --extensions openai
+python server.py --listen --listen-port $PORT --auto-devices --gpu-memory $GPUMEMORY --no-cache --cpu-memory $CPUMEMORY --threads $THREADS --rwkv-cuda-on --monkey-patch --extensions openai --model $MODEL
