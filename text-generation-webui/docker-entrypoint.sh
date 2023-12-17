16089e6f9b32cf7dcccad0d02102e043176e1dee
James Choa <triggerhappyelite@gmail.com>
2023-12-17 11:17:04 +0800

    Change enabled extension to alltalk_tts

--- text-generation-webui/docker-entrypoint.sh
+++ text-generation-webui/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 cd text-generation-webui
 #python jupyter lab --ip='*' --NotebookApp.token='' --NotebookApp.password='' &
-python server.py --listen --listen-port $PORT --auto-devices --gpu-memory $GPUMEMORY --no-cache --cpu-memory $CPUMEMORY --threads $THREADS --rwkv-cuda-on --monkey-patch --extensions openai coqui_tts --model $MODEL
+python server.py --listen --listen-port $PORT --auto-devices --gpu-memory $GPUMEMORY --no-cache --cpu-memory $CPUMEMORY --threads $THREADS --rwkv-cuda-on --monkey-patch --extensions openai alltalk_tts --model $MODEL
