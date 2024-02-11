e4cf60155b3384546d0ac43a6962273e31f736ee
James Choa <triggerhappyelite@gmail.com>
2024-02-11 10:35:28 +0800

    Rename the jupyter stuff
    Split between multi and multi-gpu

---
+++ jupyter-multi-gpu/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export GOPATH=/home/jupyter/data/go
+export PATH=$PATH:/usr/local/go/bin:/home/jupyter/data/go/bin
+cd /home/jupyter/data
+
+# Check if .jupyterlab-venv dir already exists
+if [ ! -d ".jupyterlab-venv" ]; then
+  echo "Creating virtual environment..."
+  python3 -m venv .jupyterlab-venv
+  source .jupyterlab-venv/bin/activate
+  pip install --upgrade pip
+  pip install -r ../requirements.txt
+  ~/.cargo/bin/evcxr_jupyter --install
+
+  go install github.com/janpfeifer/gonb@latest
+  go install golang.org/x/tools/cmd/goimports@latest
+  go install golang.org/x/tools/gopls@latest
+  gonb --install
+fi
+
+source .jupyterlab-venv/bin/activate
+jupyter lab --ip='*' --NotebookApp.token=$JUPYTER_TOKEN --NotebookApp.password=$JUPYTER_PASSWORD
